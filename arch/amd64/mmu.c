7c8baf2be5a6044c30bf45d3ee47a716a0009949
Anton Kling <anton@kling.gg>
2025-09-21 14:20:26 +0200

    MMU: Fix a lot of early memory allocation bootstrapping

--- arch/amd64/mmu.c
+++ arch/amd64/mmu.c
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#define PAGE_SIZE 0x1000
+
 #define PAGE_FLAG_PRESENT (1 << 0)
 
 struct PT {
@@ -27,6 +29,9 @@ struct PML4T {
   uintptr_t physical[512];
   struct PDPT *pdpt[512];
 };
+
+bool check_virtual_region_is_free(void *address, void **physical,
+                                  bool allocate);
 
 // Depends upon a C version after C99 since it uses `typeof`
 #define align_up(address, alignment)                                           \
@@ -104,34 +109,72 @@ void *get_frame(bool allocate) {
   return NULL;
 }
 
-void *allocate_address(void *address);
 void allocate_next_pdt(void *address);
+
+bool in_bootstrap_phase = true;
 
 void *heap_end;
 
-// void *mmu_allocate_free_virutal_region(size_t length) {
-// }
+void *mmu_find_free_virtual_region(size_t length) {
+  for (size_t offset = 0;; offset += PAGE_SIZE) {
+    bool is_free = true;
+    for (size_t i = 0; i < length; i += PAGE_SIZE) {
+      void *address = (void *)((uintptr_t)heap_end + offset + i);
+      if (!check_virtual_region_is_free(address, NULL, false)) {
+        is_free = false;
+        break;
+      }
+    }
+    if (is_free) {
+      return (void *)((uintptr_t)heap_end + offset);
+    }
+  }
+  assert(0);
+  return NULL;
+}
 
 // FIXME: WARNING: The allocation is not guaranteed to be linear in the
 // physical memory mapping.
 void *ksbrk_physical(size_t length, void **physical, bool a) {
   void *rc = heap_end;
+  if (!in_bootstrap_phase) {
+    heap_end = mmu_find_free_virtual_region(length);
+    rc = heap_end;
+  }
+
   if (!a) {
     allocate_next_pdt(heap_end);
+    heap_end = mmu_find_free_virtual_region(length);
     rc = heap_end;
   }
   void *r = NULL;
   for (size_t i = 0; i < length; i += 0x1000) {
-    void *l = allocate_address(heap_end);
-    assert(l);
+    void *physical;
+
+    // TODO: Maybe do something different? Current "problem" is that the
+    // page table does get fully filled, but the regions are not
+    // actually used since the kernel does not stretch that far, so we
+    // can reuse them and save a lot of pain when it comes to
+    // preallocating a table. Currently it is just using this
+    // "bootstrapping" stage where it ignores if a region already is "in
+    // use" such that it can allocate new "unused" tables for later
+    // allocations. This does also mean some frames(and address space)
+    // get lost forever, but it **should** not be that much. Maybe
+    // allocate an extra table in boot.s to avoid this hack?
+    bool was_free = check_virtual_region_is_free(heap_end, &physical, true);
+    if (!in_bootstrap_phase) {
+      assert(was_free);
+    }
+
     if (!r) {
-      r = l;
+      r = physical;
     }
     heap_end += 0x1000;
   }
   if (physical) {
     *physical = r;
   }
+  in_bootstrap_phase = false;
   return rc;
 }
 
@@ -191,6 +234,7 @@ void allocate_next_pdt(void *address) {
         PAGE_FLAG_PRESENT)) {
     void *physical;
     void *address = ksbrk_physical(sizeof(struct PT), &physical, 1);
+    memset(address, 0, sizeof(struct PT));
 
     kernel->pdpt[pml4t_index]->pdt[pdpt_index]->physical[pdt_index] =
         (uintptr_t)physical | 0x3;
@@ -201,6 +245,7 @@ void allocate_next_pdt(void *address) {
         PAGE_FLAG_PRESENT)) {
     void *physical;
     void *address = ksbrk_physical(sizeof(struct PT), &physical, 1);
+    memset(address, 0, sizeof(struct PT));
 
     kernel->pdpt[pml4t_index]->pdt[pdpt_index]->physical[pdt_index] =
         (uintptr_t)physical | 0x3;
@@ -208,6 +253,13 @@ void allocate_next_pdt(void *address) {
   }
 }
 
+// if allocate == false:
+//   Returns true if the region does not exist.
+//   Return false if the region does exist
+// if allocate == true:
+//   Returns false if region already exists
+//   Returns false if region did not allocate
+//   Return true if the region did not exist and was allocated
 bool check_virtual_region_is_free(void *address, void **physical,
                                   bool allocate) {
   const int PT_SHIFT = 12;
@@ -220,20 +272,25 @@ bool check_virtual_region_is_free(void *address, void **physical,
   uint64_t pdt_index = ((uintptr_t)address >> PDT_SHIFT) & 0x1FF;
   uint64_t pt_index = ((uintptr_t)address >> PT_SHIFT) & 0x1FF;
 
+  bool region_exists = true;
+
   if (!(kernel->physical[pml4t_index] & 0x1)) {
     kprintf("ERROR 1\n");
     assert(!allocate);
-    return false;
+    region_exists = false;
+    goto check_return;
   }
   if (!(kernel->pdpt[pml4t_index]->physical[pdpt_index] & 0x1)) {
     kprintf("ERROR 2\n");
     assert(!allocate);
-    return false;
+    region_exists = false;
+    goto check_return;
   }
   if (!(kernel->pdpt[pml4t_index]->pdt[pdpt_index]->physical[pdt_index] &
         0x1)) {
     if (!allocate) {
-      return false;
+      region_exists = false;
+      goto check_return;
     }
     void *physical;
     void *address = ksbrk_physical(sizeof(struct PT), &physical, 0);
@@ -249,22 +306,40 @@ bool check_virtual_region_is_free(void *address, void **physical,
                  ->pt[pdt_index]
                  ->page[pt_index];
 
-  if (allocate) {
-    *p = get_frame(true);
-    *p = (void *)((uintptr_t)*p | 0x3);
-  }
+  if (!(((uintptr_t)*p) & PAGE_FLAG_PRESENT)) {
+    // Region does not exist and we allocate it.
+    if (allocate) {
+      *p = get_frame(true);
+      *p = (void *)((uintptr_t)*p | 0x3);
+      if (physical) {
+        *physical = (void *)((uintptr_t)(*p) & ~(0xFFF));
+      }
 
+      return true;
+    }
+    region_exists = false;
+    goto check_return;
+  }
   if (physical) {
-    *physical = *p;
+    *physical = (void *)((uintptr_t)(*p) & ~(0xFFF));
   }
 
-  return ((((uintptr_t)*p) & PAGE_FLAG_PRESENT));
-}
-
-void *allocate_address(void *address) {
-  void *physical;
-  assert(check_virtual_region_is_free(address, &physical, true));
-  return (void *)((uintptr_t)physical & ~(0xFFF));
+check_return:
+  // if allocate == false:
+  //   Returns true if the region does not exist.
+  //   Return false if the region does exist
+  // if allocate == true:
+  //   Returns false if region already exists
+  //   Returns false if region did not allocate
+  //   Return true if the region did not exist and was allocated
+  if (!allocate) {
+    return !region_exists;
+  } else {
+    //    if (region_exists) {
+    //      return false;
+    //    }
+    return false;
+  }
 }
 
 int mmu_init(void *multiboot_header) {
