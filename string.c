61769c2d8a66d8e4acbaf576c78056a4545b77e6
Anton Kling <anton@kling.gg>
2025-09-21 14:14:08 +0200

    .

--- string.c
+++ string.c
@@ -32,7 +32,7 @@ void *memcpy(void *dest, const void *src, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
   char *p = s;
-  for (; n > 0; n--) {
+  for (; n > 0; n--, p++) {
     *p = c;
   }
   return s;
