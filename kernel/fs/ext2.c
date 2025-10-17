52cdb9c06c42cee7f57eba127346033556c29538
Anton Kling <anton@kling.gg>
2025-10-17 22:16:43 +0200

    ext2: Implement writing to a ext2 file

--- kernel/fs/ext2.c
+++ kernel/fs/ext2.c
@@ -1,19 +1,21 @@
 #include <assert.h>
+#include <drivers/ahci.h>
 #include <error.h>
 #include <fs/ext2.h>
 #include <fs/vfs.h>
 #include <kmalloc.h>
+#include <kprintf.h>
 #include <log.h>
 #include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 
-#define SECTOR_SIZE 512
-
 #define EXT2_SUPERBLOCK_SECTOR 2
 #define EXT2_ROOT_INODE 2
 
 #define BLOCKS_REQUIRED(_a, _b) ((_a) / (_b) + (((_a) % (_b)) != 0))
+
+#define INDIRECT_BLOCK_CAPACITY (ctx->block_byte_size / sizeof(u32))
 
 #define ALIGN(value, alignment)                                                \
   if (0 != (value % alignment)) {                                              \
@@ -34,12 +36,65 @@ struct ext2_ctx {
 
 static void read_block(struct ext2_ctx *ctx, u32 block, void *address,
                        size_t size, size_t offset);
+static void write_block(struct ext2_ctx *ctx, u32 block, const void *address,
+                        size_t size, size_t offset);
+static int get_free_block(struct ext2_ctx *ctx, bool allocate);
+
+static u32 num_block_groups(struct ext2_ctx *ctx) {
+  ext_superblock_t *superblock = &ctx->superblock;
+  // Determining the Number of Block Groups
+
+  // From the Superblock, extract the size of each block, the total
+  // number of inodes, the total number of blocks, the number of blocks
+  // per block group, and the number of inodes in each block group. From
+  // this information we can infer the number of block groups there are
+  // by:
+
+  // Rounding up the total number of blocks divided by the number of
+  // blocks per block group
+  u32 num_blocks = superblock->num_blocks;
+  u32 num_blocks_in_group = superblock->num_blocks_group;
+  u32 b = num_blocks / num_blocks_in_group;
+  // TODO: This calculation seems to generate a off by one error
+  // for certain ext2 filesystems, it is better to be off by one than
+  // one too many in this case so we risk leaving a group unused.
+  //  if (num_blocks % num_blocks_in_group != 0) {
+  //    b++;
+  //  }
+
+  // Rounding up the total number of inodes divided by the number of
+  // inodes per block group
+  u32 num_inodes = superblock->num_inodes;
+  u32 num_inodes_in_group = superblock->num_inodes_group;
+  u32 i = num_inodes / num_inodes_in_group;
+  // See TODO above.
+  //  if (num_inodes % num_inodes_in_group != 0) {
+  //    i++;
+  //  }
+  // Both (and check them against each other)
+  assert(i == b);
+  return i;
+}
+
+static void write_group_descriptor(struct ext2_ctx *ctx, u32 group_index,
+                                   bgdt_t *block_group) {
+  int starting_block = (1024 == ctx->block_byte_size) ? 2 : 1;
+  write_block(ctx, starting_block, (u8 *)block_group, sizeof(bgdt_t),
+              group_index * sizeof(bgdt_t));
+}
 
 static void get_group_descriptor(struct ext2_ctx *ctx, u32 group_index,
                                  bgdt_t *block_group) {
   int starting_block = (1024 == ctx->block_byte_size) ? 2 : 1;
   read_block(ctx, starting_block, block_group, sizeof(bgdt_t),
              group_index * sizeof(bgdt_t));
+}
+
+static void write_to_indirect_block(struct ext2_ctx *ctx, u32 indirect_block,
+                                    u32 index, u32 new_block) {
+  index %= INDIRECT_BLOCK_CAPACITY;
+  write_block(ctx, indirect_block, (u8 *)&new_block, sizeof(u32),
+              index * sizeof(u32));
 }
 
 static void get_block_containing_inode(struct ext2_ctx *ctx, u32 inode_index,
@@ -86,12 +141,70 @@ static int get_block(struct ext2_ctx *ctx, inode_t *inode, u32 i) {
   return 0;
 }
 
+static int allocate_block(struct ext2_ctx *ctx, inode_t *inode, u32 index,
+                          int block) {
+  if (index < 12) {
+    inode->block_pointers[index] = block;
+    return 1;
+  }
+  index -= 12;
+  if (index < INDIRECT_BLOCK_CAPACITY) {
+    if (0 == index) {
+      int n = get_free_block(ctx, true);
+      if (-1 == n) {
+        return 0;
+      }
+      inode->single_indirect_block_pointer = n;
+    }
+    write_to_indirect_block(ctx, inode->single_indirect_block_pointer, index,
+                            block);
+    return 1;
+  }
+  index -= INDIRECT_BLOCK_CAPACITY;
+  if (index < INDIRECT_BLOCK_CAPACITY * INDIRECT_BLOCK_CAPACITY) {
+    if (0 == index) {
+      int n = get_free_block(ctx, true);
+      if (-1 == n) {
+        return 0;
+      }
+      inode->double_indirect_block_pointer = n;
+    }
+
+    u32 value;
+    if (0 == (index % INDIRECT_BLOCK_CAPACITY)) {
+      int n = get_free_block(ctx, true);
+      if (-1 == n) {
+        return 0;
+      }
+      write_to_indirect_block(ctx, inode->double_indirect_block_pointer,
+                              index / INDIRECT_BLOCK_CAPACITY, n);
+      value = n;
+    } else {
+      value = get_singly_block_index(ctx, inode->double_indirect_block_pointer,
+                                     index / INDIRECT_BLOCK_CAPACITY);
+    }
+
+    write_to_indirect_block(ctx, value, index, block);
+    return 1;
+  }
+  return 0;
+}
+
 static void read_block(struct ext2_ctx *ctx, u32 block, void *address,
                        size_t size, size_t offset) {
   // TODO: Cache
   int err;
   vfs_pread(ctx->fd, address, size, block * ctx->block_byte_size + offset,
             &err);
+  assert(err == ERROR_SUCCESS);
+}
+
+static void write_block(struct ext2_ctx *ctx, u32 block, const void *address,
+                        size_t size, size_t offset) {
+  // TODO: Cache
+  int err;
+  vfs_pwrite(ctx->fd, address, size, block * ctx->block_byte_size + offset,
+             &err);
   assert(err == ERROR_SUCCESS);
 }
 
@@ -241,10 +354,7 @@ static bool find_inode(struct ext2_ctx *ctx, struct sv file, u32 *inode,
       break;
     }
 
-    // The expected returned entry is a directory
     if (TYPE_INDICATOR_DIRECTORY != a.type_indicator) {
-      // klog(LOG_WARN, "ext2: Expected diretory but got: %d",
-      // a.type_indicator);
       return false;
     }
   }
@@ -252,6 +362,147 @@ static bool find_inode(struct ext2_ctx *ctx, struct sv file, u32 *inode,
     *inode = cur_path_inode;
   }
   return true;
+}
+
+static int get_free_blocks(struct ext2_ctx *ctx, bool allocate, int entries[],
+                           u32 num_entries) {
+  u32 current_entry = 0;
+  bgdt_t block_group;
+  if (num_entries > ctx->superblock.num_blocks_unallocated) {
+    return 0;
+  }
+  assert(0 == ctx->superblock.num_blocks_group % 8);
+  for (u32 group = 0;
+       group < num_block_groups(ctx) && current_entry < num_entries; group++) {
+    get_group_descriptor(ctx, group, &block_group);
+
+    if (0 == block_group.num_unallocated_blocks_in_group) {
+      continue;
+    }
+
+    u8 bitmap[(ctx->superblock.num_blocks_group) / 8];
+    read_block(ctx, block_group.block_usage_bitmap, bitmap,
+               (ctx->superblock.num_blocks_group) / 8, 0);
+    int found_block = 0;
+    for (u32 index = 0; index < ctx->superblock.num_blocks_group / 8 &&
+                        current_entry < num_entries;
+         index++) {
+      if (0xFF == bitmap[index]) {
+        continue;
+      }
+      for (u32 offset = 0; offset < 8 && current_entry < num_entries;
+           offset++) {
+        if (bitmap[index] & (1 << offset)) {
+          continue;
+        }
+        u32 block_index =
+            index * 8 + offset + group * ctx->superblock.num_blocks_group;
+        bitmap[index] |= (1 << offset);
+        entries[current_entry] = block_index;
+        current_entry++;
+        found_block = 1;
+      }
+    }
+    if (allocate && found_block) {
+      write_block(ctx, block_group.block_usage_bitmap, bitmap,
+                  ctx->superblock.num_blocks_group / 8, 0);
+      block_group.num_unallocated_blocks_in_group--;
+      write_group_descriptor(ctx, group, &block_group);
+      ctx->superblock.num_blocks_unallocated--;
+      ctx->superblock_changed = 1;
+    }
+  }
+  return current_entry;
+}
+
+static int get_free_block(struct ext2_ctx *ctx, bool allocate) {
+  int entry[1];
+  if (0 == get_free_blocks(ctx, allocate, entry, 1)) {
+    return -1;
+  }
+  return entry[0];
+}
+
+static void write_inode_header(struct ext2_ctx *ctx, int inode_index,
+                               inode_t *data) {
+  u32 block_index;
+  u32 block_offset;
+  get_block_containing_inode(ctx, inode_index, &block_index, &block_offset);
+
+  u8 mem_block[ctx->inode_size];
+  memcpy(mem_block, data, ctx->inode_size);
+  write_block(ctx, block_index, mem_block, ctx->inode_size, block_offset);
+}
+
+static int write_inode(struct ext2_ctx *ctx, int inode_num, const void *data,
+                       u64 size, u64 offset, u64 *file_size, int append) {
+  (void)file_size;
+  u8 inode_buffer[ctx->inode_size];
+  get_inode_header(ctx, inode_num, inode_buffer);
+  inode_t *inode = (inode_t *)inode_buffer;
+
+  u64 fsize = (u64)(((u64)inode->_upper_32size << 32) | (u64)inode->low_32size);
+  if (append) {
+    offset = fsize;
+  }
+
+  u32 block_start = offset / ctx->block_byte_size;
+  u32 block_offset = offset % ctx->block_byte_size;
+
+  int num_blocks_used =
+      inode->num_disk_sectors / (ctx->block_byte_size / SECTOR_SIZE);
+
+  if (size + offset > fsize) {
+    fsize = size + offset;
+  }
+
+  u32 num_blocks_required = BLOCKS_REQUIRED(fsize, ctx->block_byte_size);
+
+  u32 delta = num_blocks_required - num_blocks_used;
+  if (delta > 0) {
+    int blocks[delta];
+    get_free_blocks(ctx, true, blocks, delta);
+    for (u32 i = num_blocks_used; i < num_blocks_required; i++) {
+      assert(allocate_block(ctx, inode, i, blocks[i - num_blocks_used]));
+    }
+  }
+
+  inode->num_disk_sectors =
+      num_blocks_required * (ctx->block_byte_size / SECTOR_SIZE);
+
+  int bytes_written = 0;
+  for (int i = block_start; size; i++) {
+    u32 block = get_block(ctx, inode, i);
+    if (0 == block) {
+      break;
+    }
+
+    int write_len = ((size + block_offset) > ctx->block_byte_size)
+                        ? (ctx->block_byte_size - block_offset)
+                        : size;
+    write_block(ctx, block, data + bytes_written, write_len, block_offset);
+    block_offset = 0;
+    bytes_written += write_len;
+    size -= write_len;
+  }
+  inode->low_32size = fsize;
+  inode->_upper_32size = (fsize >> 32);
+  write_inode_header(ctx, inode_num, inode);
+  return bytes_written;
+}
+
+size_t ext2_write(struct vfs_fd *fd, const void *buffer, size_t length,
+                  size_t offset, err_t *err) {
+  u32 inode_num = (u32)fd->internal_object;
+  /* TODO
+  assert(fd->inode->type != FS_TYPE_DIRECTORY);
+  if (fd->inode->type == FS_TYPE_LINK) {
+    inode_num = resolve_link(inode_num);
+  }
+  */
+  ASSIGN_PTR(err, ERROR_SUCCESS);
+  struct ext2_ctx *ctx = (struct ext2_ctx *)fd->mount->internal_object;
+  return write_inode(ctx, inode_num, buffer, length, offset, NULL, 0);
 }
 
 size_t ext2_read(struct vfs_fd *fd, void *buffer, size_t length, size_t offset,
@@ -303,13 +554,10 @@ struct vfs_fd *ext2_open(struct vfs_mount *mount, struct sv file, int flags,
   fd->close = NULL;
   fd->offset = 0;
   fd->read = ext2_read;
-  fd->write = NULL;
+  fd->write = ext2_write;
   fd->lseek = ext2_lseek;
   fd->type = VFS_TYPE_FILE;
   fd->internal_object = (void *)inode_num;
-
-  //  assert(p->open);
-  //  assert(true == p->open(fd, file, flags, p->internal_object, err));
 
   return fd;
 }
@@ -336,7 +584,6 @@ static bool parse_superblock(struct ext2_ctx *ctx, err_t *rc_err) {
   }
 
   ctx->inodes_per_block = ctx->block_byte_size / ctx->inode_size;
-  //  cache = kcalloc(num_block_cache, sizeof(struct block_cache));
   return true;
 }
 
