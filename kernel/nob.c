9674cea9c02187462f69ce3ff2e8e971ef94ef51
Anton Kling <anton@kling.gg>
2025-10-23 00:13:21 +0200

    meta: Add more flags to nob build system
    This allows for generation of release/fast builds of the kernel and
    makes it easier to toggle ubsan/lto and other such things without
    changing the source code.

--- kernel/nob.c
+++ kernel/nob.c
@@ -64,6 +64,27 @@ char *code_file_to_obj(char *file) {
   return obj;
 }
 
+/*
+ * This flag implies
+ * inlcude_ubsan = false;
+ * CFLAGS=-O2
+ * really_fast_build = false
+ */
+bool release_build = false;
+
+/*
+ * This flag implies
+ * inlcude_ubsan = false;
+ * CFLAGS=-O3
+ * uses_lto = true;
+ */
+bool really_fast_build = false;
+
+bool include_ubsan = true;
+
+/* Just appends -flto to LD and CC and removes stupid warnings */
+bool uses_lto = false;
+
 char *ld_flags[] = {
     "-shared",
     "-ffreestanding",
@@ -82,8 +103,6 @@ char *c_flags[] = {
     "-mno-red-zone",
     "-Wno-int-to-pointer-cast",
     "-Wno-pointer-to-int-cast",
-    "-fsanitize=vla-bound,shift-exponent,pointer-overflow,shift,signed-integer-"
-    "overflow,bounds",
     "-I./arch/includes/",
     "-I.",
     "-I../include/",
@@ -119,6 +138,21 @@ int build_c_file(char *file, char *object_output, Nob_Procs *procs,
   Nob_Cmd cmd = {0};
   nob_cmd_append(&cmd, CC, "-o", object_output, "-c", file);
   nob_da_append_many(&cmd, c_flags, ARRAY_LEN(c_flags));
+
+  if (include_ubsan) {
+    nob_cmd_append(&cmd, "-fsanitize=vla-bound,shift-exponent,pointer-overflow,"
+                         "shift,signed-integer-overflow,bounds");
+  }
+  if (release_build) {
+    nob_cmd_append(&cmd, "-O2");
+  }
+  if (really_fast_build) {
+    nob_cmd_append(&cmd, "-O3");
+  }
+  if (uses_lto) {
+    nob_cmd_append(&cmd, "-flto");
+  }
+
   if (!nob_cmd_run(&cmd, .async = procs)) {
     return 1;
   }
@@ -179,17 +213,29 @@ int create_iso_file(void) {
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
 
-  int flag_clean = 0;
-  int flag_build = 1;
-  int flag_build_override = 0;
+  bool flag_clean = false;
+  bool flag_build = true;
+  bool flag_build_override = false;
 
   for (int i = 1; i < argc; i++) {
     if (0 == strcmp(argv[i], "clean")) {
-      flag_clean = 1;
-      flag_build = 0;
+      flag_clean = true;
+      flag_build = false;
     }
     if (0 == strcmp(argv[i], "build")) {
-      flag_build_override = 1;
+      flag_build_override = true;
+    }
+    if (0 == strcmp(argv[i], "release")) {
+      release_build = true;
+    }
+    if (0 == strcmp(argv[i], "fast")) {
+      really_fast_build = true;
+    }
+    if (0 == strcmp(argv[i], "lto")) {
+      uses_lto = true;
+    }
+    if (0 == strcmp(argv[i], "no_ubsan")) {
+      include_ubsan = false;
     }
   }
   if (!flag_build) {
@@ -211,10 +257,26 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  if (release_build) {
+    really_fast_build = false;
+    include_ubsan = false;
+  }
+
+  if (really_fast_build) {
+    include_ubsan = false;
+    uses_lto = true;
+  }
+
   Nob_Cmd cmd = {0};
   nob_cmd_append(&cmd, CC, "-T", "linker.ld", "-o", TARGET ".elf");
 
   nob_da_append_many(&cmd, ld_flags, ARRAY_LEN(ld_flags));
+  if (uses_lto) {
+    nob_cmd_append(&cmd, "-flto");
+    // NOTE: GCC is stupid and can't figure out that calling arguments
+    // from assembly code are initialized.
+    nob_cmd_append(&cmd, "-Wno-maybe-uninitialized");
+  }
   nob_da_append_many(&cmd, c_flags, ARRAY_LEN(c_flags));
 
   Nob_File_Paths objects = {0};
@@ -243,7 +305,7 @@ int main(int argc, char **argv) {
       nob_needs_rebuild(TARGET ".elf", objects.items, objects.count);
   if (rebuild_is_needed) {
     nob_da_append_many(&cmd, objects.items, objects.count);
-    if (!nob_cmd_run(&cmd, .async = &procs)) {
+    if (!nob_cmd_run(&cmd)) {
       return 1;
     }
   }
