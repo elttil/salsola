365e039eb0c7e0ff458aa2fcaebade5cf2a89d0b
Anton Kling <anton@kling.gg>
2025-11-09 23:12:29 +0100

    libc: Add execv(p)

--- userland/libc/unistd/execvp.c
+++ userland/libc/unistd/execvp.c
@@ -3,17 +3,36 @@
 #include <unistd.h>
 
 int execvp(const char *file, char *const argv[]) {
-  struct sv f = C_TO_SV(file);
-
-  int argc = 0;
-  for (; argv[argc]; argc++)
-    ;
-
-  struct sv *args = allocarray(sizeof(struct sv), argc);
-  for (int i = 0; i < argc; i++) {
-    args[i] = C_TO_SV(argv[i]);
+  if ('/' == *file) {
+    return execv(file, argv);
   }
 
-  errno = error_to_errno(sa_exec(f, args, argc));
+  char *p = getenv("PATH");
+  if (!p) {
+    errno = ENOENT;
+    return -1;
+  }
+
+  struct sv paths = C_TO_SV(p);
+
+  struct sb builder;
+  sb_init(&builder);
+  for (;;) {
+    struct sv path = sv_split_delim(paths, &paths, ':');
+    if (0 == sv_length(path)) {
+      break;
+    }
+    sb_reset(&builder);
+    sb_append_sv(&builder, path);
+    sb_append(&builder, "/");
+    sb_append(&builder, file);
+    sb_append_buffer(&builder, "\0", 1);
+
+    if (-1 == execv(builder.string, argv)) {
+      continue;
+    }
+  }
+  sb_free(&builder);
+  errno = ENOENT;
   return -1;
 }
