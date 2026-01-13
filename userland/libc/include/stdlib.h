45046adfb8ae21eb826c856197612852cefa1bdb
Anton Kling <anton@kling.gg>
2026-01-13 18:24:33 +0100

    libc: Move getenv/setenv to stdlib
    I have no clue why I thought it was stdio

---
+++ userland/libc/include/stdlib.h
@@ -0,0 +1,39 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <limits.h>
+#include <stddef.h>
+#define RAND_MAX (UINT32_MAX)
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+typedef size_t size_t; // only for 32 bit
+
+void *malloc(size_t s);
+void *calloc(size_t nelem, size_t elsize);
+void *allocarray(size_t nmemb, size_t size);
+void *reallocarray(void *ptr, size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+void free(void *p);
+int rand(void);
+void srand(unsigned int seed);
+unsigned long strtoul(const char *restrict str, char **restrict endptr,
+                      int base);
+long strtol(const char *str, char **restrict endptr, int base);
+long long strtoll(const char *str, char **restrict endptr, int base);
+void abort(void);
+int abs(int i);
+void qsort(void *base, size_t nel, size_t width,
+           int (*compar)(const void *, const void *));
+int atexit(void (*func)(void));
+int mkstemp(char *template);
+long double strtold(const char *restrict nptr, char **restrict endptr);
+int system(const char *command);
+double atof(const char *str);
+double strtod(const char *restrict nptr, char **restrict endptr);
+int atoi(const char *str);
+char *realpath(const char *filename, char *resolvedname);
+long atol(const char *nptr);
+long long atoll(const char *nptr);
+int setenv(const char *name, const char *value, int overwrite);
+char *getenv(const char *name);
+#endif
