215dc6fa7f171d1285dbd35b928975daa3ac19ce
Anton Kling <anton@kling.gg>
2026-01-15 00:41:05 +0100

    libc: Fix crt0 to call exit after return from main

--- userland/libc/setup.c
+++ userland/libc/setup.c
@@ -1,7 +1,25 @@
+#include <assert.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <sys/random.h>
 
-void _libc_setup() {
+FILE *__stdin_FILE;
+FILE *__stdout_FILE;
+FILE *__stderr_FILE;
+#define stdin __stdin_FILE
+#define stdout __stdout_FILE
+#define stderr __stderr_FILE
+
+int main(int argc, char **argv);
+
+void _libc_setup(int argc, char **argv) {
   sarandom_init(NULL);
   sarandom_set_aggressive_key_overwriting(NULL);
+
+  __stdin_FILE = fdopen(0, "r");
+  __stdout_FILE = fdopen(1, "w");
+  __stderr_FILE = fdopen(2, "w");
+
+  exit(main(argc, argv));
+  assert(0);
 }
