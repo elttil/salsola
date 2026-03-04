1b5c40874413d828e5efd3e64a0b45ae78097d92
Anton Kling <anton@kling.gg>
2026-03-04 15:52:20 +0100

    libc: Ensure alignment for memcpy

--- userland/libc/string/memcpy.c
+++ userland/libc/string/memcpy.c
@@ -4,16 +4,26 @@ void *memcpy(void *dest, const void *src, uint32_t n) {
   unsigned char *d = dest;
   const unsigned char *s = src;
 
-  for (; n >= 8; n -= 8, d += 8, s += 8) {
-    *(uint64_t *)d = *(uint64_t *)s;
+  for (; (0 != ((uintptr_t)s % 8)) && n; n--) {
+    *d++ = *s++;
   }
 
-  for (; n >= 4; n -= 4, d += 4, s += 4) {
-    *(uint32_t *)d = *(uint32_t *)s;
+  if ((uintptr_t)d % 8 == 0) {
+    for (; n >= 8; n -= 8, d += 8, s += 8) {
+      *(uint64_t *)d = *(uint64_t *)s;
+    }
   }
 
-  for (; n >= 2; n -= 2, d += 2, s += 2) {
-    *(uint16_t *)d = *(uint16_t *)s;
+  if ((uintptr_t)d % 4 == 0) {
+    for (; n >= 4; n -= 4, d += 4, s += 4) {
+      *(uint32_t *)d = *(uint32_t *)s;
+    }
+  }
+
+  if ((uintptr_t)d % 2 == 0) {
+    for (; n >= 2; n -= 2, d += 2, s += 2) {
+      *(uint16_t *)d = *(uint16_t *)s;
+    }
   }
   for (; n; n--) {
     *d++ = *s++;
